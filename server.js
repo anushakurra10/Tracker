53d37bab59b7026af494f7fc9c2bea8b6039a675
anushakurra10 <68759543+anushakurra10@users.noreply.github.com>
2020-12-16 18:21:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ const app = express();
 app.use(express.static(__dirname + '/dist/tracking'));
 // Start the app by listening on the default
 // Heroku port
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/tracking', 'index.html'));
+});
 app.listen(process.env.PORT || 8080);
